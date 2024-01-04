4ad1bac8fc625c30f15a54008076e2f2384bea3d
reshma patel <youremail@address.com>
2024-01-04 15:28:14 +0530

    new file added....

---
+++ nginx_dsl.groovy
@@ -0,0 +1,21 @@
+pipelineJob("Build-Deploy) {
+ definition {
+  cpsScm{
+ scm{
+ git{
+
+remote{
+
+url("https://github.com/reshma910/jenkins.git")
+credentials("")
+
+}
+branch('*/master')
+}
+}
+scriptPath("Jenkins_ecr")
+}
+
+}
+
+}
